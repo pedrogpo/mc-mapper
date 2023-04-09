a2c4859d1a554914ca09c617e9e7f51fb873d1d5
Pedro Guilherme <pedroguilhermepedromen47@gmail.com>
2023-04-09 03:36:37 -0300

    feat: add fields

--- cmd/auto-mapper/main.go
+++ cmd/auto-mapper/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/pedrogpo/mc-auto-mapper/internal/builder"
 	"github.com/pedrogpo/mc-auto-mapper/internal/constants"
 	csvutil "github.com/pedrogpo/mc-auto-mapper/internal/utils/csv"
 	"github.com/pedrogpo/mc-auto-mapper/internal/utils/generics"
@@ -19,56 +19,10 @@ import (
 
 var rootPath string = "data/mappings/"
 
-type Mappings struct {
-	classes map[string]constants.Map
-	fields  map[string]constants.Map
-	methods map[string]constants.MethodMap
-}
-
-var allMappings Mappings = Mappings{
-	classes: make(map[string]constants.Map),
-	fields:  make(map[string]constants.Map),
-	methods: make(map[string]constants.MethodMap),
-}
-
-var classesMutex sync.Mutex
-
-func (m *Mappings) addClass(clsName string, obfName string, clsPath string, minecraftVersion string) {
-	classesMutex.Lock()
-	if _, ok := m.classes[clsName]; !ok {
-		m.classes[clsName] = constants.Map{
-			ObfMappings: []constants.VersionMap{
-				{
-					Version: minecraftVersion,
-					Name:    obfName,
-				},
-			},
-			SrgMappings: []constants.VersionMap{
-				{
-					Version: minecraftVersion,
-					Name:    clsPath,
-				},
-			},
-		}
-	} else {
-		obfMappings := m.classes[clsName].ObfMappings
-		obfMappings = append(obfMappings, constants.VersionMap{
-			Version: minecraftVersion,
-			Name:    obfName,
-		})
-
-		srgMappings := m.classes[clsName].SrgMappings
-		srgMappings = append(srgMappings, constants.VersionMap{
-			Version: minecraftVersion,
-			Name:    clsPath,
-		})
-
-		m.classes[clsName] = constants.Map{
-			ObfMappings: obfMappings,
-			SrgMappings: srgMappings,
-		}
-	}
-	classesMutex.Unlock()
+var allMappings constants.Mappings = constants.Mappings{
+	Classes: make(map[string]constants.Map),
+	Fields:  make(map[string](map[string]constants.Map)),
+	Methods: make(map[string](map[string]constants.MethodMap)),
 }
 
 func handleJoinedType(line string, fieldsCsv *csvutil.CSV, methodsCsv *csvutil.CSV, minecraftVersion string) {
@@ -83,14 +37,14 @@ func handleJoinedType(line string, fieldsCsv *csvutil.CSV, methodsCsv *csvutil.C
 		clsSplitted := strings.Split(clsPath, "/")
 		clsName := clsSplitted[len(clsSplitted)-1]
 
-		allMappings.addClass(clsName, obfName, clsPath, minecraftVersion)
+		allMappings.AddClass(clsName, obfName, clsPath, minecraftVersion)
 	case joined.FIELD:
 		// Get the second row from the CSV file
-		// obfSplitted := strings.Split(parts[1], "/")
-		// obfName := obfSplitted[1]
+		obfSplitted := strings.Split(parts[1], "/")
+		obfName := obfSplitted[1]
 
 		srgSplitted := strings.Split(parts[2], "/")
-		// clsFromName := srgSplitted[len(srgSplitted)-2]
+		clsFromName := srgSplitted[len(srgSplitted)-2]
 		srgName := srgSplitted[len(srgSplitted)-1]
 
 		fieldRow := fieldsCsv.GetRowIdx(srgName)
@@ -99,8 +53,9 @@ func handleJoinedType(line string, fieldsCsv *csvutil.CSV, methodsCsv *csvutil.C
 			return
 		}
 
-		// fieldName := fieldRow[1]
+		fieldName := fieldRow[1]
 
+		allMappings.AddField(clsFromName, fieldName, obfName, srgName, minecraftVersion)
 	case joined.METHOD:
 		// fmt.Println("METHOD")
 	}
@@ -137,60 +92,6 @@ func mapForVersion(minecraftVersion string) {
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
-}
-
-func createClassesFile() {
-	mappingsClasses := strings.Builder{}
-	mappingsClasses.WriteString("const std::map<const char*, s_class_info> mappings_classes = {")
-
-	for clsName, clsMap := range allMappings.classes {
-		if !generics.Contains(constants.ClassesToMap, clsName) {
-			continue
-		}
-
-		mappingsClasses.WriteString("{\"" + clsName + "\",")
-		mappingsClasses.WriteString("{{")
-
-		tryList := strings.Builder{}
-		grouped := make(map[string][]string)
-		for _, t := range clsMap.SrgMappings {
-			grouped[t.Name] = append(grouped[t.Name], t.Version)
-		}
-
-		for key, value := range grouped {
-			ss := strings.Builder{}
-			ss.WriteString(fmt.Sprintf("{\"%s\", {", key))
-			for _, v := range value {
-				ss.WriteString(fmt.Sprintf("\"%s\",", v))
-			}
-			ss.WriteString("}},")
-			tryList.WriteString(ss.String())
-		}
-
-		tryListStr := strings.TrimSuffix(tryList.String(), ",")
-
-		mappingsClasses.WriteString(tryListStr)
-
-		mappingsClasses.WriteString("},{")
-
-		for _, obfMapping := range clsMap.ObfMappings {
-			mappingsClasses.WriteString("{\"" + obfMapping.Version + "\", \"" + obfMapping.Name + "\"},")
-		}
-
-		mappingsClasses.WriteString("}}")
-		mappingsClasses.WriteString("},")
-	}
-
-	mappingsClasses.WriteString("};")
-
-	err := ioutil.WriteFile("out/classes.txt", []byte(mappingsClasses.String()), 0644)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println("Classes file created")
 }
 
 func main() {
@@ -233,7 +134,11 @@ func main() {
 
 	fmt.Println("Finished")
 
-	createClassesFile()
+	// create out/classes.txt
+	builder.CreateClassesFile(allMappings)
+
+	// create out/fields.txt
+	builder.CreateFieldsFile(allMappings)
 
 	// para o timer
 	fim := time.Now()
