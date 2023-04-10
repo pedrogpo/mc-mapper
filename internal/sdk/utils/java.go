a8badecc5b26ef286a2666458720c20f47a009ae
Pedro Guilherme <pedroguilhermepedromen47@gmail.com>
2023-04-10 03:24:00 -0300

    refac: changed sdk stuff

--- internal/sdk/utils/java.go
+++ internal/sdk/utils/java.go
@@ -1,6 +1,10 @@
 package sdkutils
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/pedrogpo/mc-auto-mapper/internal/utils/java"
+)
 
 func GetReturnTypeForSDK(returnType string) (string, bool) {
 	isSDKType := false
@@ -11,53 +15,9 @@ func GetReturnTypeForSDK(returnType string) (string, bool) {
 		returnType = "sdk::" + returnType
 		isSDKType = true
 	} else if len(returnType) > 1 && returnType[0] == '[' {
-		// Handle array types
-		switch returnType[1:] {
-		case "Z":
-			returnType = "jbooleanArray"
-		case "B":
-			returnType = "jbyteArray"
-		case "C":
-			returnType = "jcharArray"
-		case "S":
-			returnType = "jshortArray"
-		case "I":
-			returnType = "jintArray"
-		case "J":
-			returnType = "jlongArray"
-		case "F":
-			returnType = "jfloatArray"
-		case "D":
-			returnType = "jdoubleArray"
-		case "V":
-			returnType = "voidArray"
-		default:
-			returnType = "jobjectArray"
-		}
+		returnType = java.HandleJNIArrayType(returnType[1:])
 	} else {
-		// Handle non-array types
-		switch returnType {
-		case "Z":
-			returnType = "jboolean"
-		case "B":
-			returnType = "jbyte"
-		case "C":
-			returnType = "jchar"
-		case "S":
-			returnType = "jshort"
-		case "I":
-			returnType = "jint"
-		case "J":
-			returnType = "jlong"
-		case "F":
-			returnType = "jfloat"
-		case "D":
-			returnType = "jdouble"
-		case "V":
-			returnType = "void"
-		default:
-			returnType = "jobject"
-		}
+		returnType = java.HandleJNINonArrayType(returnType)
 	}
 	return returnType, isSDKType
 }
--- internal/sdk/utils/methods.go
+++ internal/sdk/utils/methods.go
@@ -21,54 +21,35 @@ func RemoveDuplicateSigs(methodMap constants.MethodMap) []constants.MethodsSig {
 		}
 	}
 
-	result := make([]constants.MethodsSig, 0, len(uniqueMethods))
+	result := make([]constants.MethodsSig, len(uniqueMethods))
+	i := 0
 	for _, sig := range uniqueMethods {
-		result = append(result, sig)
+		result[i] = sig
+		i++
 	}
 
 	return result
 }
 
 func GenerateMethodFunctionForCPPFile(returnType string, methodName string, isSDKType bool, paramList []string) string {
-	params := ""
+	var builder strings.Builder
 
 	for i := range paramList {
-		params += ", param" + strconv.Itoa(i)
+		builder.WriteString(", param")
+		builder.WriteString(strconv.Itoa(i))
 	}
+
+	params := builder.String()
 
 	if isSDKType {
 		return `const auto obj = this->env->CallObjectMethod(this->instance, g_mapper->methods["` + methodName + `"]` + params + `);
 	return std::make_shared<` + returnType + `>(obj);`
 	}
 
-	functionType := ""
+	functionType := java.GetJNIFunctionType(returnType)
 
 	if len(returnType) > 1 && returnType[0] == '[' {
 		functionType = "CallObjectMethod"
-	} else {
-		// Handle non-array types
-		switch returnType {
-		case "jboolean":
-			functionType = "CallBooleanMethod"
-		case "jbyte":
-			functionType = "CallByteMethod"
-		case "jchar":
-			functionType = "CallCharMethod"
-		case "jshort":
-			functionType = "CallShortMethod"
-		case "jint":
-			functionType = "CallIntMethod"
-		case "jlong":
-			functionType = "CallLongMethod"
-		case "jfloat":
-			functionType = "CallFloatMethod"
-		case "jdouble":
-			functionType = "CallDoubleMethod"
-		case "void":
-			functionType = "CallVoidMethod"
-		default:
-			functionType = "CallObjectMethod"
-		}
 	}
 
 	return `return this->env->` + functionType + `(this->instance, g_mapper->methods["` + methodName + `"]` + params + `);`
